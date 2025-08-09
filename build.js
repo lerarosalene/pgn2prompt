ed1600f1e49100663870952d3cbf665e97833375
Larissa Rosalene <lerarosalene@outlook.com>
2025-08-10 02:30:07 +0300

    add favicon and attribution; minify html

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 const p = require("node:path");
 const fs = require("node:fs");
 const esbuild = require("esbuild");
+const {minify} = require("html-minifier-terser");
 const sass = require("sass");
 
 const fsp = fs.promises;
@@ -14,12 +15,21 @@ async function main() {
     outfile: p.join("dist", "index.bundle.js"),
   });
 
-  await fsp.copyFile(p.join("src", "index.html"), p.join("dist", "index.html"));
+  const html = await fsp.readFile(p.join("src", "index.html"), "utf-8");
+  const compressed = await minify(html, { collapseWhitespace: true });
+  await fsp.writeFile(p.join("dist", "index.html"), compressed);
 
   const { css } = sass.compile(p.join("src", "index.scss"), {
     style: "compressed",
   });
   await fsp.writeFile(p.join("dist", "index.bundle.css"), css);
+
+  for (const icon of await fsp.readdir("icons")) {
+    if (!icon.endsWith(".png")) {
+      continue;
+    }
+    await fsp.copyFile(p.join("icons", icon), p.join("dist", icon));
+  }
 }
 
 main().catch((error) => {
---
+++ icons/attribution.md
@@ -0,0 +1,1 @@
+[Chessboard](https://icons8.com/icon/4YZ2KEF3PN7c/chessboard) icon by [Icons8](https://icons8.com)

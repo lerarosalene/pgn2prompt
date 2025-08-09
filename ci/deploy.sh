75fd823ee034928a37454fddba410c10c583a06b
Larissa Rosalene <lerarosalene@outlook.com>
2025-08-10 01:54:55 +0300

    netlify

---
+++ ci/deploy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -ex
+
+if [ -z "${NETLIFY_TOKEN}" ]; then
+  echo "Pass NETLIFY_TOKEN as environment variable. Aborting."
+  exit 1
+fi
+
+if [ -z "${NETLIFY_SITE_ID}" ]; then
+  echo "Pass NETLIFY_SITE_ID as environment variable. Aborting."
+  exit 1
+fi
+
+DIST_NAME="dist.zip"
+
+npm ci
+npm run tsc
+npm run build
+
+if [ -f "${DIST_NAME}" ]; then
+  rm "${DIST_NAME}"
+fi
+
+pushd dist
+  zip -r9 "../${DIST_NAME}" .
+popd
+
+curl \
+  -H"Content-Type: application/zip" \
+  -H"Authorization: Bearer $NETLIFY_TOKEN" \
+  -XPOST \
+  --data-binary "@${DIST_NAME}" \
+  "https://api.netlify.com/api/v1/sites/${NETLIFY_SITE_ID}/deploys"
